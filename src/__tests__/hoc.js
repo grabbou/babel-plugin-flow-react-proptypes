b90f518418cac31cc871128c293a9ad073f99e7d
Mike Grabowski <grabbou@gmail.com>
2017-04-14 11:06:29 +0200

    Fix indent

--- src/__tests__/hoc.js
+++ src/__tests__/hoc.js
@@ -5,10 +5,10 @@ type FooProps = {
 };
 
 export default () => {
-	class C extends React.Component {
+  class C extends React.Component {
     props: FooProps;
-	}
-	return C;
+  }
+  return C;
 };
 `;
 
@@ -18,6 +18,6 @@ it('import-object', () => {
     presets: ['es2015', 'stage-1', 'react'],
     plugins: ['syntax-flow', require('../')],
   }).code;
-	console.log(res);
+  console.log(res);
   expect(res).toMatchSnapshot();
 });
